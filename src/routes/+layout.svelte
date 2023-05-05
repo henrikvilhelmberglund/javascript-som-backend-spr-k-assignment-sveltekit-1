223c845d0e355df32948c405fc749e2767775c3f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-05 21:44:31 +0200

    feat: :sparkles: add layout with nav and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script>
+	import Footer2 from "$lib/Footer2.svelte";
 	import "@unocss/reset/antfu.css";
 </script>
 
+<header>
+	<nav class="bg-slate-300 p-2">
+		<a class="p-4 text-2xl text-blue-700 hover:text-blue-500" href="/">Home</a>
+		<a class="p-4 text-2xl text-blue-700 hover:text-blue-500" href="/members">Members</a>
+		<a class="p-4 text-2xl text-blue-700 hover:text-blue-500" href="/add-member"> Add member</a>
+	</nav>
+</header>
+
 <slot />
+
+<Footer2 />
 
 <style uno:preflights uno:safelist global></style>
