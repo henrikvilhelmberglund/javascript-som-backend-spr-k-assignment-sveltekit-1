d5e6b6b1b2abf9c7748ebb7dd7f64af5b57556b0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-05 21:43:45 +0200

    feat: :sparkles: add Footer2

---
+++ src/lib/Footer2.svelte
@@ -0,0 +1,4 @@
+<footer class="fixed bottom-0 w-full bg-slate-200 p-4">
+	<a class="text-2xl text-blue-600 underline" href="mailto:memberlist@memberlist.com">Contact us</a>
+	<p>Copyright Henrik 2023</p>
+</footer>
