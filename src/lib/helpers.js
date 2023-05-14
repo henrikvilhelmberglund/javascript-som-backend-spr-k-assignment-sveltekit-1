3ba14f481edcbcf517a3f8089af6f7af63dda48f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-14 15:14:30 +0200

    feat: :sparkles: add toNiceDate function

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,6 +1,21 @@
 import { browser } from "$app/environment";
 import { get } from "svelte/store";
 import { activeTheme, preferredMode } from "./stores";
+
+export const toNiceDate = (dateObject) => {
+	// Assuming you have a Date object
+	const date = new Date(dateObject);
+
+	// Extract the year, month, and day components
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0"); // Add 1 to the month since it is zero-based
+	const day = String(date.getDate()).padStart(2, "0");
+
+	// Format the components into the desired output
+	const formattedDate = `${year}-${month}-${day}`;
+	return formattedDate;
+};
+
 export function activateTheme(theme, override) {
 	if (browser) {
 		if (!override) {
